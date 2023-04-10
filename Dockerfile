cf7590ef397d7351cec115118373d1c33de9a70b
hellopahe <hellopanghe@gmail.com>
2023-04-10 19:49:48 +0800

    bug fix: change mysql to mongodb

--- Dockerfile
+++ Dockerfile
@@ -5,11 +5,12 @@ COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 
 # 把这个替换成自己的telegram token
-ENV ACCESS_TOKEN=""
+ENV ACCESS_TOKEN="6178080288:AAH_avla6yFLcRFRse8G7zX28dOC0MX5Lew"
 
 ENV user="root"
 ENV pwd="cptbtptp"
 ENV sqlhost=51.120.244.80
 ENV db="comp"
+ENV mongodb="mongodb://cloiud-computing:sVwNXL7xMF7R1RLC1zPXgECh2upmcPFuyt3J3KxnFpZYmDdKbbVmNGj4TjR1gQkuhDSVZdGeIVOHACDbXgylzA==@cloiud-computing.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@cloiud-computing@"
 
 CMD ["python", "projectchatbot.py"]
--- projectchatbot.py
+++ projectchatbot.py
@@ -1,69 +1,33 @@
 import random, requests, openai, threading, logging, os, re, datetime, signal
 import mysql.connector
-
+import pymongo
 from mysql.connector import errorcode
 from datetime import timedelta, datetime
 from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton, ReplyKeyboardMarkup
 
+## use mongoDB
+connection_string = os.environ['mongodb']
+client = pymongo.MongoClient(connection_string, ssl=True)
 
-## sql connector
-config = {
-    'user': os.environ['user'],
-    'password': os.environ['pwd'],
-    'host': os.environ['sqlhost'],
-    'database': os.environ['db']
-}
-global cnx, cursor
+# Access a database and a collection
+database = client['mydatabase']
+collection = database['mycollection']
 
-class TimeoutException(Exception):
-    pass
-
-def timer(timeout_event, timeout):
-    timeout_event.wait(timeout)
-    if not timeout_event.is_set():
-        timeout_event.set()
-        raise TimeoutException('Execution timed out')
-
-def mysql_connection():
-    global cnx, cursor
-    try:
-        cnx = mysql.connector.connect(**config)
-    except mysql.connector.Error as err:
-        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-            logging.info("Something is wrong with your user name or password")
-        elif err.errno == errorcode.ER_BAD_DB_ERROR:
-            logging.info("Database does not exist")
-        else:
-            logging.info(err)
-    else:
-        logging.info("MySQL connection successful")
-        cursor = cnx.cursor(buffered=True)
 
 def pingsql():
-    timeout_event = threading.Event()
-    timeout_thread = threading.Thread(target=timer, args=(timeout_event, 3))
-    try:
-        timeout_thread.start()
-        cursor.execute("SELECT * from User_info_test_1 where 1;")
-        timeout_event.set()
-        logging.info('good connection')
-    except:
-        mysql_connection()
-        logging.info('reconnected')
-    finally:
-        timeout_thread.join()
-        if timeout_event.is_set():
-            logging.info('Execution completed within timeout')
+    # global cursor
+    # cursor = get_cursor()
+    pass
 
 # handle functions
 def error(update, context):
     logging.warning('Update "%s" caused error "%s"', update, context.error)
 
 def start(update, context):
+    logging.info('用户点击了/start')
     user_id = update.message.from_user.id
     user_nickname = update.message.from_user.username
-    sql_update_user_info(user_id, user_nickname)
 
     reply_keyboard = [
         [KeyboardButton('/start')],
@@ -106,6 +70,7 @@ def genres(update, context):
 
 # function to handle button click
 def button(update, context):
+    logging.info('用户点击了genre按钮')
     query = update.callback_query
 
     # genre = json.loads(query.data)['with']
@@ -135,6 +100,7 @@ def button(update, context):
 
 # function to handle movie button click
 def movie_button(update, context):
+    logging.info('用户点击了电影详情')
     query = update.callback_query
     movie_id = query.data
     logging.info("movie_query: ")
@@ -162,60 +128,44 @@ def movie_button(update, context):
     except Exception as e:
         query.message.reply_text("This movie was not supported by TMDB API")
 
-def director_query(update, context):
-    query = update.callback_query
-    director = query.data.split('_')[1]
-
-    response = requests.get(f'https://api.themoviedb.org/3/search/person?api_key=bfa1c4b7acab32a4eb75aa244f15754f&query={director}')
-
-    director_id = response.json()['results'][0]['id']
-    name = response.json()['results'][0]['name']
-    bio = response.json()['results'][0]['biography']
-    profile_url = f"https://image.tmdb.org/t/p/w500{response.json()['results'][0]['profile_path']}"
-
-    # send message with director details
-    message = f"<b>Name:</b> {name}\n<b>Biography:</b> {bio}"
-
-    keyboard = [[InlineKeyboardButton("Top 3 Movies", callback_data=f'top_movies_{director_id}')]]
-    reply_markup = InlineKeyboardMarkup(keyboard)
-
-    # send message with director profile picture
-    query.message.reply_photo(photo=profile_url, caption=message, parse_mode="HTML", reply_markup=reply_markup)
-
-
-## User profile management
-def sql_update_user_info(user_id, user_nickname):
-    pingsql()
-    now = datetime.now()
-    timestamp = now.strftime('%Y-%m-%d %H:%M:%S')
-    query = "INSERT INTO User_info_test_1 (user_id, user_nickname, user_last_active) " \
-            "VALUES (%s, %s, %s) " \
-            "ON DUPLICATE KEY UPDATE " \
-            "user_nickname = VALUES(user_nickname), " \
-            "user_last_active = VALUES(user_last_active)"
-    values = (user_id, user_nickname, timestamp)
-    cursor.execute(query, values)
-    cnx.commit()
 
 def sql_add_user_fav(user_id, movie_id):
-    sql_query = "INSERT INTO User_favorite_test_1 (user_id, movie_id) " \
-                "VALUES (%s, %s) " \
-                "ON DUPLICATE KEY UPDATE user_id = user_id"
-    values = (user_id, movie_id)
-    pingsql()
-    cursor.execute(sql_query, values)
-    cnx.commit()
+    # sql_query = "INSERT INTO User_favorite_test_1 (user_id, movie_id) " \
+    #             "VALUES (%s, %s) " \
+    #             "ON DUPLICATE KEY UPDATE user_id = user_id;"
+    # values = (user_id, movie_id)
+    # pingsql()
+    # cursor.execute(sql_query, values)
+    # cnx.commit()
+
+    # mongodb
+    data = {'user_id':user_id, 'movie_id': movie_id}
+    collection.insert_one(data)
+
+
 
 def sql_get_user_fav(user_id):
-    pingsql()
-    query = "SELECT movie_id FROM User_favorite_test_1 WHERE user_id = %s ORDER BY RAND() LIMIT 3"
-    values = (user_id,)
-    pingsql()
-    cursor.execute(query, values)
-    result = cursor.fetchall()
-    return result
+    # query = "SELECT movie_id FROM User_favorite_test_1 WHERE user_id = %s ORDER BY RAND() LIMIT 3;"
+    # values = (user_id,)
+    # pingsql()
+    # cursor.execute(query, values)
+    # result = cursor.fetchall()
+    # return result
+
+    ## mongoDB
+    query = {'user_id': user_id}
+    results = collection.find(query)
+
+    ans = []
+    for result in results:
+        ans.append(result['movie_id'])
+
+    return ans
+
+
 
 def add_to_fav(update, context):
+    logging.info('用户点击了add to favorite')
     query = update.callback_query
     movie_id = query.data.split('_')[1]
     logging.info(movie_id)
@@ -224,6 +174,7 @@ def add_to_fav(update, context):
     query.message.reply_text("added to favorite successfully")
 
 def get_from_fav(update, context):
+    logging.info('用户点击了/get')
     query = update.callback_query
     logging.info("get_from_fav" )
     results = sql_get_user_fav(update.effective_user.id)
@@ -233,7 +184,7 @@ def get_from_fav(update, context):
 
     else:
         try:
-            result = random.choice(results)[0]
+            result = random.choice(results)
             print("bingo" + str(result))
 
             # get movie details from API based on selected movie id
@@ -247,9 +198,9 @@ def get_from_fav(update, context):
             poster_url = f"https://image.tmdb.org/t/p/w500{response.json()['poster_path']}"
 
             message = f"<b>Title:</b> {title}\n<b>Duration:</b> {duration}\n<b>Director:</b> {director}\n<b>Cast:</b> {', '.join(cast)}"
-            context.bot.send_photo(chat_id=update.effective_user.id, photo=poster_url, caption=message, parse_mode="HTML")
+            context.bot.send_photo(chat_id=update.effective_user.id, photo=poster_url, caption=message[:200], parse_mode="HTML")
         except Exception as e:
-            context.bot.send_message(chat_id=update.effective_user.id, test="We got unexpected problem")
+            context.bot.send_message(chat_id=update.effective_user.id, text="We got unexpected problem")
 
 ## GPT features
 # local vars for GPT
@@ -285,6 +236,7 @@ def find_a_working_key():
     logging.info('done finding keys')
 
 def ask_neko_of_movies(update, context):
+    logging.info('用户点击了/ask')
     query = update.callback_query
     logging.info("ask_neko_of_movies")
 
@@ -301,7 +253,7 @@ def ask_neko_of_movies(update, context):
     else:
         # context.bot.send_photo(chat_id=update.effective_user.id, photo=poster_url, caption=message, parse_mode="HTML")
         for ele in results:
-            ids.append(ele[0])
+            ids.append(ele)
 
         for id in ids:
             # get movie details from API based on selected movie id
@@ -368,6 +320,10 @@ def ask_neko_of_movies(update, context):
             context.bot.send_message(text="Oops! we got a problem with GPT Api", chat_id=update.effective_user.id)
             logging.info("ask_neko_of_movies replied: Oops! we got a problem with GPT Api")
 
+def wake_up_sql():
+    # cursor.execute("show tables;")
+    global cnx
+
 
 def main():
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
@@ -379,6 +335,7 @@ def main():
     dp.add_handler(CommandHandler('genres', genres))
     dp.add_handler(CommandHandler('get', get_from_fav))
     dp.add_handler(CommandHandler('ask', ask_neko_of_movies))
+    # dp.add_handler(CommandHandler('askgpt', askgpt))
     dp.add_handler(CallbackQueryHandler(button, pattern=re.compile(r'^(action|comedy|drama|horror)$')))
     # dp.add_handler(CallbackQueryHandler(director_query, pattern=re.compile(r'^director_(.+)$')))
     dp.add_handler(CallbackQueryHandler(add_to_fav, pattern=re.compile(r'^fav_(\d+)$')))
@@ -392,12 +349,15 @@ def main():
     find_a_working_key()
     t = threading.Timer(1200, find_a_working_key)
     t.start()
+    #
+    # k = threading.Timer(180, wake_up_sql)
+    # k.start()
 
     # # sql
-    # s = threading.Timer(600, pingsql)
+    # s = threading.Timer(20, pingsql)
     # s.start()
 
-    mysql_connection()
+    # mysql_connection()
 
     # start the bot
     updater.start_polling()
